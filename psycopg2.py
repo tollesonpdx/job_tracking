55a22513949f7fd0bae639d03165f4164e8fddd7
Chad Tolleson <tolleson@pdx.edu>
2021-11-10 10:51:18 -0800

    made some minor changes to the psycopg2 py file

--- psycopg2.py
+++ psycopg2.py
@@ -1,47 +1,29 @@
-import time
-import os
 import psycopg2
 from configurations import getConfiguration
-# import configparser
-# from flask import Flask, request
-# from flask_sqlalchemy import SQLAlchemy
 
-# __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-
-# def config(filename=(os.path.join(__location__,'configs/database.ini')), section='postgresql'):
-#     parser = configparser.ConfigParser()  # creating a parser
-#     parser.read(filename)  # reading the config file, using default values is nothing is specified
-#     connection_parameters = {}
-#     if parser.has_section(section):
-#         params = parser.items(section)
-#         for param in params:
-#             connection_parameters[param[0]] = param[1]
-#     else:
-#         raise Exception(f'Section {section} not found in the "{filename}" file')
-#     return connection_parameters
-
-def connect():
+def connectToPSQL():
     """ Connect to the PostgreSQL database server """
-    conn = None
+    connection = None
     try:
-        params = getConfiguration.config('database.ini', 'postgresql')
-        print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(**params)
-        cur = conn.cursor()  # creating a cursor
+        parameters = getConfiguration.config('database.ini', 'postgresql')
+        print(f'Connecting to the PostgreSQL database using these parameters: {parameters}')
+        connection = psycopg2.connect(**parameters)
+        # connection = psycopg2.connect()
+        cursor = connection.cursor()  # creating a cursor
         print('PostgreSQL database version:')
-        cur.execute("""SELECT version()""")
-        db_version = cur.fetchone()
+        cursor.execute("""SELECT version()""")
+        db_version = cursor.fetchone()
         print(db_version)
-        cur.close()  # close the PG connection
+        cursor.close()  # close the PG connection
     except (Exception) as error:
         print(f'There was an error when connecting: {error}')
     finally:
-        if conn is not None:
-            conn.close()
+        if connection is not None:
+            connection.close()
             print('Database connection closed.')
 
 
 
 if __name__ == '__main__':
 
-    connect()
+    connectToPSQL()
